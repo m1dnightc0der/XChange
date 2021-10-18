m1dnightc0der/XChange

commit 69580a6b2601287eab2ceb6c3036a73ce32e40af

Removed deprecated wapi. Added sandbox/production support and more integration tests

xchange-binance/src/main/java/org/knowm/xchange/binance/BinanceAuthenticated.java
@@ -1,7 +1,5 @@
 package org.knowm.xchange.binance;
 
-import static org.knowm.xchange.binance.BinanceResilience.*;
-
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
@@ -285,11 +283,11 @@ public interface BinanceAuthenticated extends Binance {
       throws IOException, BinanceException;
 
   @POST
-  @Path("wapi/v3/withdraw.html")
+  @Path("/sapi/v1/capital/withdraw/apply")
   /**
    * Submit a withdraw request.
    *
-   * @param asset
+   * @param coin
    * @param address
    * @param addressTag optional for Ripple
    * @param amount
@@ -303,23 +301,23 @@ public interface BinanceAuthenticated extends Binance {
    * @throws BinanceException
    */
   WithdrawRequest withdraw(
-      @QueryParam("asset") String asset,
-      @QueryParam("address") String address,
-      @QueryParam("addressTag") String addressTag,
-      @QueryParam("amount") BigDecimal amount,
-      @QueryParam("name") String name,
-      @QueryParam("recvWindow") Long recvWindow,
-      @QueryParam("timestamp") SynchronizedValueFactory<Long> timestamp,
+      @FormParam("coin") String coin,
+      @FormParam("address") String address,
+      @FormParam("addressTag") String addressTag,
+      @FormParam("amount") BigDecimal amount,
+      @FormParam("name") String name,
+      @FormParam("recvWindow") Long recvWindow,
+      @FormParam("timestamp") SynchronizedValueFactory<Long> timestamp,
       @HeaderParam(X_MBX_APIKEY) String apiKey,
       @QueryParam(SIGNATURE) ParamsDigest signature)
       throws IOException, BinanceException;
 
   @GET
-  @Path("wapi/v3/depositHistory.html")
+  @Path("/sapi/v1/capital/deposit/hisrec")
   /**
    * Fetch deposit history.
    *
-   * @param asset optional
+   * @param coin optional
    * @param startTime optional
    * @param endTime optional
    * @param recvWindow optional
@@ -330,8 +328,8 @@ public interface BinanceAuthenticated extends Binance {
    * @throws IOException
    * @throws BinanceException
    */
-  DepositList depositHistory(
-      @QueryParam("asset") String asset,
+  List<BinanceDeposit> depositHistory(
+      @QueryParam("coin") String coin,
       @QueryParam("startTime") Long startTime,
       @QueryParam("endTime") Long endTime,
       @QueryParam("recvWindow") Long recvWindow,
@@ -341,11 +339,11 @@ public interface BinanceAuthenticated extends Binance {
       throws IOException, BinanceException;
 
   @GET
-  @Path("wapi/v3/withdrawHistory.html")
+  @Path("/sapi/v1/capital/withdraw/history")
   /**
    * Fetch withdraw history.
    *
-   * @param asset optional
+   * @param coin optional
    * @param startTime optional
    * @param endTime optional
    * @param recvWindow optional
@@ -356,8 +354,8 @@ public interface BinanceAuthenticated extends Binance {
    * @throws IOException
    * @throws BinanceException
    */
-  WithdrawList withdrawHistory(
-      @QueryParam("asset") String asset,
+  List<BinanceWithdraw> withdrawHistory(
+      @QueryParam("coin") String coin,
       @QueryParam("startTime") Long startTime,
       @QueryParam("endTime") Long endTime,
       @QueryParam("recvWindow") Long recvWindow,
@@ -378,7 +376,7 @@ public interface BinanceAuthenticated extends Binance {
    * @throws BinanceException
    */
   @GET
-  @Path("/wapi/v3/userAssetDribbletLog.html")
+  @Path("/sapi/v1/asset/dribblet ")
   AssetDribbletLogResponse userAssetDribbletLog(
       @QueryParam("recvWindow") Long recvWindow,
       @QueryParam("timestamp") SynchronizedValueFactory<Long> timestamp,
@@ -413,9 +411,9 @@ public interface BinanceAuthenticated extends Binance {
       throws IOException, BinanceException;
 
   @GET
-  @Path("/wapi/v3/sub-account/transfer/history.html")
-  TransferHistoryResponse transferHistory(
-      @QueryParam("email") String email,
+  @Path("/sapi/v1/sub-account/sub/transfer/history")
+  List<TransferHistory> transferHistory(
+      @QueryParam("fromEmail") String fromEmail,
       @QueryParam("startTime") Long startTime,
       @QueryParam("endTime") Long endTime,
       @QueryParam("page") Integer page,
@@ -441,11 +439,11 @@ public interface BinanceAuthenticated extends Binance {
       throws IOException, BinanceException;
 
   @GET
-  @Path("wapi/v3/depositAddress.html")
+  @Path("/sapi/v1/capital/deposit/address")
   /**
    * Fetch deposit address.
    *
-   * @param asset
+   * @param coin
    * @param recvWindow
    * @param timestamp
    * @param apiKey
@@ -455,7 +453,7 @@ public interface BinanceAuthenticated extends Binance {
    * @throws BinanceException
    */
   DepositAddress depositAddress(
-      @QueryParam("asset") String asset,
+      @QueryParam("coin") String coin,
       @QueryParam("recvWindow") Long recvWindow,
       @QueryParam("timestamp") SynchronizedValueFactory<Long> timestamp,
       @HeaderParam(X_MBX_APIKEY) String apiKey,
@@ -463,7 +461,7 @@ public interface BinanceAuthenticated extends Binance {
       throws IOException, BinanceException;
 
   @GET
-  @Path("wapi/v3/assetDetail.html")
+  @Path("/sapi/v1/asset/assetDetail")
   /**
    * Fetch asset details.
    *
@@ -475,7 +473,7 @@ public interface BinanceAuthenticated extends Binance {
    * @throws IOException
    * @throws BinanceException
    */
-  AssetDetailResponse assetDetail(
+  Map<String, AssetDetail> assetDetail(
       @QueryParam("recvWindow") Long recvWindow,
       @QueryParam("timestamp") SynchronizedValueFactory<Long> timestamp,
       @HeaderParam(X_MBX_APIKEY) String apiKey,

xchange-binance/src/main/java/org/knowm/xchange/binance/dto/account/ApiResponse.java
@@ -1,13 +1,12 @@
 package org.knowm.xchange.binance.dto.account;
 
-public abstract class WapiResponse<T> {
+public abstract class ApiResponse<T> {
 
   public final boolean success;
   public final String
       msg; // "{\"code\":-1021,\"msg\":\"Timestamp for this request is not valid.\"}"
 
-  public WapiResponse(boolean success, String msg) {
-    super();
+  public ApiResponse(boolean success, String msg) {
     this.success = success;
     this.msg = msg;
   }

xchange-binance/src/main/java/org/knowm/xchange/binance/dto/account/AssetDribbletLogResponse.java
@@ -11,7 +11,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 public final class AssetDribbletLogResponse
-    extends WapiResponse<AssetDribbletLogResponse.AssetDribbletLogResults> {
+    extends ApiResponse<AssetDribbletLogResponse.AssetDribbletLogResults> {
 
   private final AssetDribbletLogResults results;
 

xchange-binance/src/main/java/org/knowm/xchange/binance/dto/account/WithdrawRequest.java
@@ -2,7 +2,7 @@ package org.knowm.xchange.binance.dto.account;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public final class WithdrawRequest extends WapiResponse<String> {
+public final class WithdrawRequest extends ApiResponse<String> {
   public final String id;
 
   public WithdrawRequest(

xchange-binance/src/main/java/org/knowm/xchange/binance/service/BinanceAccountService.java
@@ -39,6 +39,22 @@ public class BinanceAccountService extends BinanceAccountServiceRaw implements A
     super(exchange, binance, resilienceRegistries);
   }
 
+  private static FundingRecord.Status transferHistoryStatus(String historyStatus) {
+    Status status;
+    switch (historyStatus) {
+      case "SUCCESS":
+        status = Status.COMPLETE;
+        break;
+      default:
+        status =
+            Status.resolveStatus(historyStatus); // FIXME not documented yet in Binace spot api docs
+        if (status == null) {
+          status = Status.FAILED;
+        }
+    }
+    return status;
+  }
+
   /** (0:Email Sent,1:Cancelled 2:Awaiting Approval 3:Rejected 4:Processing 5:Failure 6Completed) */
   private static FundingRecord.Status withdrawStatus(int status) {
     switch (status) {
@@ -174,7 +190,7 @@ public class BinanceAccountService extends BinanceAccountServiceRaw implements A
 
   public Map<String, AssetDetail> getAssetDetails() throws IOException {
     try {
-      return super.requestAssetDetail().getAssetDetail();
+      return super.requestAssetDetail();
     } catch (BinanceException e) {
       throw BinanceErrorAdapter.adapt(e);
     }
@@ -255,7 +271,7 @@ public class BinanceAccountService extends BinanceAccountServiceRaw implements A
                           w.getAddress(),
                           w.getAddressTag(),
                           new Date(w.getApplyTime()),
-                          Currency.getInstance(w.getAsset()),
+                          Currency.getInstance(w.getCoin()),
                           w.getAmount(),
                           w.getId(),
                           w.getTxId(),
@@ -322,7 +338,7 @@ public class BinanceAccountService extends BinanceAccountServiceRaw implements A
                           .setCurrency(Currency.getInstance(a.getAsset()))
                           .setAmount(a.getQty())
                           .setType(Type.INTERNAL_WITHDRAWAL)
-                          .setStatus(Status.COMPLETE)
+                          .setStatus(transferHistoryStatus(a.getStatus()))
                           .build());
                 });
       }

commit 74912c895593113a91f800b4edd230d390675180

[binance] Fix withdraw response parsing (#4286)
* [binance] Fix withdraw response parsing

The bug seems to be introduced in wapi removal in 69580a6b2601287eab2ceb6c3036a73ce32e40af
The withdraw response parsing failed with: "BinanceException: null (HTTP status code: 0)"
The ApiResponse was assuming different json response format. The actual responses were added in unit test.

AssetDribbletLogResponse was using the same ApiResponse parent class (which was probably not
functioning properly) and it wasn't used anywhere so I removed that too.

* [binance] withdraw fix

rename WithdrawRequest to WithdrawResponse

AccountServiceTest does not extend AbstractResilienceTest

xchange-binance/src/main/java/org/knowm/xchange/binance/BinanceAuthenticated.java
@@ -300,7 +300,7 @@ public interface BinanceAuthenticated extends Binance {
    * @throws IOException
    * @throws BinanceException
    */
-  WithdrawRequest withdraw(
+  WithdrawResponse withdraw(
       @FormParam("coin") String coin,
       @FormParam("address") String address,
       @FormParam("addressTag") String addressTag,
@@ -358,26 +358,6 @@ public interface BinanceAuthenticated extends Binance {
       @QueryParam("coin") String coin,
       @QueryParam("startTime") Long startTime,
       @QueryParam("endTime") Long endTime,
-      @QueryParam("recvWindow") Long recvWindow,
-      @QueryParam("timestamp") SynchronizedValueFactory<Long> timestamp,
-      @HeaderParam(X_MBX_APIKEY) String apiKey,
-      @QueryParam(SIGNATURE) ParamsDigest signature)
-      throws IOException, BinanceException;
-
-  /**
-   * Fetch small amounts of assets exchanged BNB records.
-   *
-   * @param recvWindow optional
-   * @param timestamp
-   * @param apiKey
-   * @param signature
-   * @return
-   * @throws IOException
-   * @throws BinanceException
-   */
-  @GET
-  @Path("/sapi/v1/asset/dribblet ")
-  AssetDribbletLogResponse userAssetDribbletLog(
       @QueryParam("recvWindow") Long recvWindow,
       @QueryParam("timestamp") SynchronizedValueFactory<Long> timestamp,
       @HeaderParam(X_MBX_APIKEY) String apiKey,

xchange-binance/src/main/java/org/knowm/xchange/binance/dto/account/ApiResponse.java
@@ -1,15 +1,0 @@
-package org.knowm.xchange.binance.dto.account;
-
-public abstract class ApiResponse<T> {
-
-  public final boolean success;
-  public final String
-      msg; // "{\"code\":-1021,\"msg\":\"Timestamp for this request is not valid.\"}"
-
-  public ApiResponse(boolean success, String msg) {
-    this.success = success;
-    this.msg = msg;
-  }
-
-  public abstract T getData();
-}

xchange-binance/src/main/java/org/knowm/xchange/binance/dto/account/AssetDribbletLogResponse.java
@@ -1,86 +1,0 @@
-package org.knowm.xchange.binance.dto.account;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.knowm.xchange.binance.BinanceAdapters;
-
-public final class AssetDribbletLogResponse
-    extends ApiResponse<AssetDribbletLogResponse.AssetDribbletLogResults> {
-
-  private final AssetDribbletLogResults results;
-
-  public AssetDribbletLogResponse(
-      @JsonProperty("results") AssetDribbletLogResults results,
-      @JsonProperty("success") boolean success,
-      @JsonProperty("msg") String msg) {
-    super(success, msg);
-    this.results = results;
-  }
-
-  @Override
-  public AssetDribbletLogResults getData() {
-    return results;
-  }
-
-  @Override
-  public String toString() {
-    return "AssetDribbletLogResult [results="
-        + results
-        + ", success="
-        + success
-        + ", msg="
-        + msg
-        + "]";
-  }
-
-  @Data
-  @NoArgsConstructor
-  public static class AssetDribbletLogResults {
-    private BigDecimal total;
-    private BnbExchange[] rows;
-
-    public List<BnbExchange> getData() {
-      return Arrays.asList(rows);
-    }
-  }
-
-  @Data
-  @NoArgsConstructor
-  public static class BnbExchange {
-    private String transfered_total;
-    private String service_charge_total;
-    private BigInteger tran_id;
-    private BnbExchangeLog[] logs;
-    private String operate_time;
-
-    public List<BnbExchangeLog> getData() {
-      return Arrays.asList(logs);
-    }
-
-    public LocalDateTime getOperateTime() {
-      return BinanceAdapters.toLocalDateTime(operate_time);
-    }
-  }
-
-  @Data
-  @NoArgsConstructor
-  public static class BnbExchangeLog {
-    private BigInteger tranId;
-    private String serviceChargeAmount;
-    private String uid;
-    private String amount;
-    private String operateTime;
-    private String transferedAmount;
-    private String fromAsset;
-
-    public LocalDateTime getOperateTime() {
-      return BinanceAdapters.toLocalDateTime(operateTime);
-    }
-  }
-}

xchange-binance/src/main/java/org/knowm/xchange/binance/dto/account/WithdrawRequest.java
@@ -1,20 +1,0 @@
-package org.knowm.xchange.binance.dto.account;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public final class WithdrawRequest extends ApiResponse<String> {
-  public final String id;
-
-  public WithdrawRequest(
-      @JsonProperty("success") boolean success,
-      @JsonProperty("msg") String msg,
-      @JsonProperty("id") String id) {
-    super(success, msg);
-    this.id = id;
-  }
-
-  @Override
-  public String getData() {
-    return id;
-  }
-}

xchange-binance/src/main/java/org/knowm/xchange/binance/dto/account/WithdrawResponse.java
@@ -1,0 +1,15 @@
+package org.knowm.xchange.binance.dto.account;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public final class WithdrawResponse {
+    public final String id;
+
+    public WithdrawResponse(@JsonProperty("id") String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}

xchange-binance/src/main/java/org/knowm/xchange/binance/service/BinanceAccountService.java
@@ -17,6 +17,7 @@ import org.knowm.xchange.binance.dto.BinanceException;
 import org.knowm.xchange.binance.dto.account.AssetDetail;
 import org.knowm.xchange.binance.dto.account.BinanceAccountInformation;
 import org.knowm.xchange.binance.dto.account.DepositAddress;
+import org.knowm.xchange.binance.dto.account.WithdrawResponse;
 import org.knowm.xchange.client.ResilienceRegistries;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.currency.CurrencyPair;
@@ -127,7 +128,7 @@ public class BinanceAccountService extends BinanceAccountServiceRaw implements A
   public String withdrawFunds(Currency currency, BigDecimal amount, String address)
       throws IOException {
     try {
-      return super.withdraw(currency.getCurrencyCode(), address, amount);
+      return super.withdraw(currency.getCurrencyCode(), address, amount).getId();
     } catch (BinanceException e) {
       throw BinanceErrorAdapter.adapt(e);
     }
@@ -145,11 +146,11 @@ public class BinanceAccountService extends BinanceAccountServiceRaw implements A
       if (!(params instanceof DefaultWithdrawFundsParams)) {
         throw new IllegalArgumentException("DefaultWithdrawFundsParams must be provided.");
       }
-      String id = null;
+      WithdrawResponse withdraw;
       if (params instanceof RippleWithdrawFundsParams) {
         RippleWithdrawFundsParams rippleParams = null;
         rippleParams = (RippleWithdrawFundsParams) params;
-        id =
+        withdraw =
             super.withdraw(
                 rippleParams.getCurrency().getCurrencyCode(),
                 rippleParams.getAddress(),
@@ -157,14 +158,14 @@ public class BinanceAccountService extends BinanceAccountServiceRaw implements A
                 rippleParams.getAmount());
       } else {
         DefaultWithdrawFundsParams p = (DefaultWithdrawFundsParams) params;
-        id =
+        withdraw =
             super.withdraw(
                 p.getCurrency().getCurrencyCode(),
                 p.getAddress(),
                 p.getAddressTag(),
                 p.getAmount());
       }
-      return id;
+      return withdraw.getId();
     } catch (BinanceException e) {
       throw BinanceErrorAdapter.adapt(e);
     }

xchange-binance/src/main/java/org/knowm/xchange/binance/service/BinanceAccountServiceRaw.java
@@ -3,7 +3,6 @@ package org.knowm.xchange.binance.service;
 import static org.knowm.xchange.binance.BinanceResilience.REQUEST_WEIGHT_RATE_LIMITER;
 import static org.knowm.xchange.client.ResilienceRegistries.NON_IDEMPOTENT_CALLS_RETRY_CONFIG_NAME;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
@@ -34,26 +33,24 @@ public class BinanceAccountServiceRaw extends BinanceBaseService {
         .call();
   }
 
-  public String withdraw(String coin, String address, BigDecimal amount)
+  public WithdrawResponse withdraw(String coin, String address, BigDecimal amount)
       throws IOException, BinanceException {
     // the name parameter seams to be mandatory
     String name = address.length() <= 10 ? address : address.substring(0, 10);
     return withdraw(coin, address, null, amount, name);
   }
 
-  public String withdraw(String coin, String address, String addressTag, BigDecimal amount)
+  public WithdrawResponse withdraw(String coin, String address, String addressTag, BigDecimal amount)
       throws IOException, BinanceException {
     // the name parameter seams to be mandatory
     String name = address.length() <= 10 ? address : address.substring(0, 10);
     return withdraw(coin, address, addressTag, amount, name);
   }
 
-  private String withdraw(
+  private WithdrawResponse withdraw(
       String coin, String address, String addressTag, BigDecimal amount, String name)
       throws IOException, BinanceException {
-    WithdrawRequest result =
-        decorateApiCall(
-                () ->
+      return decorateApiCall(() ->
                     binance.withdraw(
                         coin,
                         address,
@@ -67,8 +64,6 @@ public class BinanceAccountServiceRaw extends BinanceBaseService {
             .withRetry(retry("withdraw", NON_IDEMPOTENT_CALLS_RETRY_CONFIG_NAME))
             .withRateLimiter(rateLimiter(REQUEST_WEIGHT_RATE_LIMITER), 5)
             .call();
-    checkApiResponse(result);
-    return result.getData();
   }
 
   public DepositAddress requestDepositAddress(Currency currency) throws IOException {
@@ -127,18 +122,6 @@ public class BinanceAccountServiceRaw extends BinanceBaseService {
         .withRetry(retry("withdrawHistory"))
         .withRateLimiter(rateLimiter(REQUEST_WEIGHT_RATE_LIMITER))
         .call();
-  }
-
-  public AssetDribbletLogResponse.AssetDribbletLogResults getAssetDribbletLog()
-      throws BinanceException, IOException {
-    return decorateApiCall(
-            () ->
-                binance.userAssetDribbletLog(
-                    getRecvWindow(), getTimestampFactory(), super.apiKey, super.signatureCreator))
-        .withRetry(retry("userAssetDribbletLog"))
-        .withRateLimiter(rateLimiter(REQUEST_WEIGHT_RATE_LIMITER))
-        .call()
-        .getData();
   }
 
   public List<AssetDividendResponse.AssetDividend> getAssetDividend(Long startTime, Long endTime)
@@ -202,18 +185,5 @@ public class BinanceAccountServiceRaw extends BinanceBaseService {
         .withRetry(retry("transferSubUserHistory"))
         .withRateLimiter(rateLimiter(REQUEST_WEIGHT_RATE_LIMITER))
         .call();
-  }
-
-  private <T> T checkApiResponse(ApiResponse<T> result) {
-    if (!result.success) {
-      BinanceException exception;
-      try {
-        exception = new ObjectMapper().readValue(result.msg, BinanceException.class);
-      } catch (Throwable e) {
-        exception = new BinanceException(-1, result.msg);
-      }
-      throw exception;
-    }
-    return result.getData();
   }
 }

xchange-binance/src/test/java/org/knowm/xchange/binance/service/account/AccountServiceTest.java
@@ -1,0 +1,70 @@
+package org.knowm.xchange.binance.service.account;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.knowm.xchange.ExchangeFactory;
+import org.knowm.xchange.ExchangeSpecification;
+import org.knowm.xchange.binance.BinanceExchange;
+import org.knowm.xchange.currency.Currency;
+import org.knowm.xchange.exceptions.ExchangeSecurityException;
+import org.knowm.xchange.service.account.AccountService;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+public class AccountServiceTest {
+
+    @Rule
+    public final WireMockRule wireMockRule = new WireMockRule();
+
+    @Test(timeout = 2000)
+    public void withdrawSuccess() throws Exception {
+        String response = withdraw("withdraw-200.json", 200);
+        assertThat(response).isEqualTo("9c7662xxxxxxxxxc8bd");
+    }
+
+    @Test(timeout = 2000)
+    public void withdrawFailure() {
+        Throwable exception = catchThrowable(() -> withdraw("withdraw-400.json", 400));
+        assertThat(exception).isInstanceOf(ExchangeSecurityException.class).hasMessage("error message (HTTP status code: 400)");
+    }
+
+    private String withdraw(String responseFileName, int statusCode) throws IOException {
+        BinanceExchange exchange = createExchange();
+        AccountService service = exchange.getAccountService();
+        stubWithdraw(responseFileName, statusCode);
+
+        return service.withdrawFunds(Currency.BTC, BigDecimal.TEN, "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa");
+    }
+
+
+    private void stubWithdraw(String fileName, int statusCode) {
+        stubFor(
+                post(urlPathEqualTo("/sapi/v1/capital/withdraw/apply"))
+                        .willReturn(
+                                aResponse()
+                                        .withStatus(statusCode)
+                                        .withHeader("Content-Type", "application/json")
+                                        .withBodyFile(fileName)));
+    }
+
+    private BinanceExchange createExchange() {
+        BinanceExchange exchange = ExchangeFactory.INSTANCE.createExchangeWithoutSpecification(BinanceExchange.class);
+        ExchangeSpecification specification = exchange.getDefaultExchangeSpecification();
+        specification.setHost("localhost");
+        specification.setSslUri("http://localhost:" + wireMockRule.port() + "/");
+        specification.setPort(wireMockRule.port());
+        specification.setShouldLoadRemoteMetaData(false);
+        specification.setHttpReadTimeout(1000);
+        exchange.applySpecification(specification);
+        return exchange;
+    }
+}