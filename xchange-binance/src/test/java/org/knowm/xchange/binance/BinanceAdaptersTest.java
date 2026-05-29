74d23a9fc0575ff71f040dff268600cc0907695d
d.melvilleclarke <d.melvilleclarke@aq.io>
2026-05-29 23:23:41 +0100

    * Update functions to replay bars directly from exchange candles

--- xchange-binance/src/test/java/org/knowm/xchange/binance/BinanceAdaptersTest.java
+++ xchange-binance/src/test/java/org/knowm/xchange/binance/BinanceAdaptersTest.java
@@ -6,13 +6,42 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import org.junit.Test;
 import org.knowm.xchange.binance.dto.account.AssetDividendResponse;
+import org.knowm.xchange.binance.dto.trade.BinanceNewOrder;
 import org.knowm.xchange.binance.dto.trade.BinanceOrder;
+import org.knowm.xchange.binance.dto.trade.OrderStatus;
+import org.knowm.xchange.binance.dto.trade.TimeInForce;
 import org.knowm.xchange.binance.service.BinanceTradeService.BinanceOrderFlags;
 import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.trade.MarketOrder;
 import org.knowm.xchange.utils.ObjectMapperHelper;
 
 public class BinanceAdaptersTest {
+
+  @Test
+  public void testUsdMarginedFuturesOrderParsesPlainStringTimeInForce() throws IOException {
+    String json =
+        "{\"orderId\":1024466142987,\"symbol\":\"BTCUSDT\",\"status\":\"CANCELED\",\"clientOrderId\":\"5ecf3a\",\"price\":\"70000.00\",\"avgPrice\":\"0.00\",\"origQty\":\"0.010\",\"executedQty\":\"0.000\",\"cumQuote\":\"0.00000\",\"timeInForce\":\"GTC\",\"type\":\"LIMIT\",\"reduceOnly\":false,\"closePosition\":false,\"side\":\"BUY\",\"positionSide\":\"BOTH\",\"stopPrice\":\"0.00\",\"workingType\":\"CONTRACT_PRICE\",\"priceProtect\":false,\"origType\":\"LIMIT\",\"priceMatch\":\"NONE\",\"selfTradePreventionMode\":\"EXPIRE_MAKER\",\"goodTillDate\":0,\"time\":1780051688232,\"updateTime\":1780051701501}";
+
+    BinanceOrder binanceOrder = ObjectMapperHelper.readValue(json, BinanceOrder.class);
+
+    assertThat(binanceOrder.orderId).isEqualTo(1024466142987L);
+    assertThat(binanceOrder.clientOrderId).isEqualTo("5ecf3a");
+    assertThat(binanceOrder.getTimeInForceEnum()).isEqualTo(TimeInForce.GTC);
+    assertThat(binanceOrder.status).isEqualTo(OrderStatus.CANCELED);
+  }
+
+  @Test
+  public void testNewOrderParsesPlainStringTimeInForce() throws IOException {
+    String json =
+        "{\"symbol\":\"BTCUSDT\",\"orderId\":1024466142987,\"clientOrderId\":\"5ecf3a\",\"transactTime\":1780051688232,\"price\":\"70000.00\",\"origQty\":\"0.010\",\"executedQty\":\"0.000\",\"status\":\"NEW\",\"timeInForce\":\"GTC\",\"type\":\"LIMIT\",\"side\":\"BUY\",\"fills\":[]}";
+
+    BinanceNewOrder binanceNewOrder = ObjectMapperHelper.readValue(json, BinanceNewOrder.class);
+
+    assertThat(binanceNewOrder.orderId).isEqualTo(1024466142987L);
+    assertThat(binanceNewOrder.clientOrderId).isEqualTo("5ecf3a");
+    assertThat(binanceNewOrder.getTimeInForceEnum()).isEqualTo(TimeInForce.GTC);
+    assertThat(binanceNewOrder.status).isEqualTo(OrderStatus.NEW);
+  }
 
   @Test
   public void testFilledMarketOrder() throws IOException {
